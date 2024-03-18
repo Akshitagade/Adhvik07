3627ba8567dc74174b005aa8b549ff64f5a57704
user.email <LENOVO@DESKTOP-85JCQID>
2024-03-18 15:39:32 +0530

    Automation Exercise

--- src/test/Signup.java
+++ src/test/Signup.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
